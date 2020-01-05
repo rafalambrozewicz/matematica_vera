e7724be9a4e7cd107c71013e04afa0ac5a9ec327
Rafal Ambrozewicz <rafal.ambrozewicz@gmail.com>
2020-01-05 18:17:56 +0100

    Added app colors and styles

---
+++ lib/config/app_color.dart
@@ -0,0 +1,7 @@
+
+import 'package:flutter/material.dart';
+
+class AppColor {
+  static const black = Color(0xde000000);
+  static const gray = Color(0x99000000);
+}
---
+++ lib/config/app_style.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:matematica_vera/config/app_color.dart';
+
+class AppStyle {
+
+  static const listFirstLine = TextStyle(
+      color: AppColor.black,
+      fontSize: 16.0
+  );
+
+  static const listSecondaryLine = TextStyle(
+      color: AppColor.gray,
+      fontSize: 14.0
+  );
+}
