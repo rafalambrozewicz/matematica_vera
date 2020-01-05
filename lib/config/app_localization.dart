a64079a3728e1293a4552fb01a6c19ab68e55a0e
Rafal Ambrozewicz <rafal.ambrozewicz@gmail.com>
2020-01-05 18:17:17 +0100

    Added localizations

---
+++ lib/config/app_localization.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+class AppLocalization {
+
+  final Locale locale;
+
+  AppLocalization(this.locale);
+
+  static AppLocalization of(BuildContext context) {
+    return Localizations.of<AppLocalization>(context, AppLocalization);
+  }
+
+  static Map<String, Map<String, String>> _localizedValues = {
+    'en': {
+      'app_name': 'matematica vera',
+      'addition': 'Addition',
+      'subtraction': 'Subtraction',
+      'range': 'Range:',
+      'exercises': 'exercises',
+      'never_done': 'Never done before',
+      'in_progress': 'In progress',
+      'last_done': 'Last done on',
+      'exercise_done': 'Exercise done!',
+      'good_job': 'Good job!',
+      'go_back': 'GO BACK',
+      'repeat': 'REPEAT',
+    },
+    'pl': {
+      'app_name': 'matematica vera',
+      'addition': 'Dodawanie',
+      'subtraction': 'Odejmowanie',
+      'range': 'Zakres:',
+      'exercises': 'ćwiczeń',
+      'never_done': 'Nigdy nie wykonano',
+      'in_progress': 'W trakcie',
+      'last_done': 'Zrobiono',
+      'exercise_done': 'Ćwicznie wykonane!',
+      'good_job': 'Dobra robota!',
+      'go_back': 'WSTECZ',
+      'repeat': 'POWTÓRZ',
+    },
+    'it': { //TODO update italian texts
+      'app_name': 'matematica vera',
+      'addition': 'Addition',
+      'subtraction': 'Subtraction',
+      'range': 'Range:',
+      'exercises': 'exercises',
+      'never_done': 'Never done before',
+      'in_progress': 'In progress',
+      'last_done': 'Last done on',
+      'exercise_done': 'Exercise done!',
+      'good_job': 'Good job!',
+      'go_back': 'GO BACK',
+      'repeat': 'REPEAT',
+    },
+  };
+
+  String get app_name => _localizedValues[locale.languageCode]['app_name'];
+  String get addition => _localizedValues[locale.languageCode]['addition'];
+  String get subtraction => _localizedValues[locale.languageCode]['subtraction'];
+  String get range => _localizedValues[locale.languageCode]['range'];
+  String get exercises => _localizedValues[locale.languageCode]['app_name'];
+  String get never_done => _localizedValues[locale.languageCode]['never_done'];
+  String get in_progress => _localizedValues[locale.languageCode]['in_progress'];
+  String get last_done => _localizedValues[locale.languageCode]['last_done'];
+  String get exercise_done => _localizedValues[locale.languageCode]['exercise_done'];
+  String get good_job => _localizedValues[locale.languageCode]['good_job'];
+  String get go_back => _localizedValues[locale.languageCode]['go_back'];
+  String get repeat => _localizedValues[locale.languageCode]['repeat'];
+}
+
+class AppLocalizationDelegate extends LocalizationsDelegate<AppLocalization> {
+  const AppLocalizationDelegate();
+
+  @override
+  bool isSupported(Locale locale) => ['en', 'pl', 'it'].contains(locale.languageCode);
+
+  @override
+  Future<AppLocalization> load(Locale locale) {
+    return SynchronousFuture<AppLocalization>(AppLocalization(locale));
+  }
+
+  @override
+  bool shouldReload(AppLocalizationDelegate old) => false;
+}
